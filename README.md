844615ec14191b88c13332e8d8c5bfa83ad2ba37
Santhoshkumar Nagulanchi <santhoshkumarnagulanchi@Santhoshkumars-MacBook-Pro.local>
2023-04-25 08:59:21 +0800

    refactored

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 Please refer to the provided document for the code challenge requirements. 
 
 ## Available scripts
-
+- `npm install` - install dependencies  
 - `npm start` - Start the application (Port 3000)
 - `npm test` - Runs available tests
+
+Corresponding Backend Code Repo: 
+https://github.com/santhosh34/code-challenge-server-go   ( master/main branch)
+ 
--- src/App copy.js
+++ src/App copy.js
@@ -44,14 +44,22 @@ function App() {
 
   useEffect(() => {
     const request = () =>
-        fetch(`http://localhost:8081/products`)
+      data.forEach((product) => {
+        fetch(`http://localhost:8081/temperature/${product.id}`)
           .then((response) => response.json())
-          .then((response) =>{
-            setItems(() => response)
-          }
-          
+          .then((response) =>
+            setItems((prevItems) => ({
+              ...prevItems,
+              [product.id]: {
+                ...product,
+                ...response,
+              },
+            }))
           );
+      });
+
     setInterval(request, 5000);
+
     request();
   }, []);
 
@@ -61,23 +69,26 @@ function App() {
       <table>
         <thead>
           <tr>
-          <th align="left">Product Id </th>
-            <th align="left">Product Name</th>
-            <th align="left">Min Temperature</th>
-            <th align="left">Max Temperature</th>
-            <th align="left">Current Temperature</th>
-            <th align="left"> Temperature Range Status</th>
+            <th align="left">Product</th>
+            <th align="left">Temperature</th>
+            <th align="left">Status</th>
           </tr>
         </thead>
         <tbody>
           {Object.keys(items).map((itemKey) => (
             <tr key={items[itemKey].id}>
-              <td width={150}>{items[itemKey].id}</td>
               <td width={150}>{items[itemKey].name}</td>
-              <td width={150}>{items[itemKey].minTemperature}</td>
-              <td width={150}>{items[itemKey].maxTemperature}</td>
               <td width={150}>{items[itemKey].temperature}</td>
-              <td width={250}>{items[itemKey].tempRangeStatus}</td>
+              <td width={150}>
+                {items[itemKey].temperature <
+                  items[itemKey].minimumTemperature && <span>too low</span>}
+                {items[itemKey].temperature >
+                  items[itemKey].maximumTemperature && <span>too high</span>}
+                {items[itemKey].temperature <=
+                  items[itemKey].maximumTemperature &&
+                  items[itemKey].temperature >=
+                    items[itemKey].minimumTemperature && <span>all good</span>}
+              </td>
             </tr>
           ))}
         </tbody>
